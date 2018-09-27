4c03731ecc5e1be579ed2bdec3c4e503aeadfedc
HayleyMa <36799593+HayleyMa@users.noreply.github.com>
2018-09-27 14:15:09 -0500

    Update BSTreeAPI.java

--- TreeTime/BSTreeAPI.java
+++ TreeTime/BSTreeAPI.java
@@ -1,19 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Describes a binary search tree<br>
+ * Requires JDK 1.8 for Function
  */
 package TreeTime;
 
 import java.util.function.Function;
-/**
- * Describes a binary search tree<br>
- * Requires JDK 1.8 for Function
- * @author Duncan
- * @since 99-99-9999
- * @param <E> the data type
- * @see BSTreeException
- */
+
 public interface BSTreeAPI<E extends Comparable<E>>
 {
    /**
