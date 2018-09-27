4c6ba31fa887e8f7b48dae6efaef9b3313c00331
HayleyMa <36799593+HayleyMa@users.noreply.github.com>
2018-09-27 14:15:58 -0500

    Update AVLTreeAPI.java

--- TreeTime/AVLTreeAPI.java
+++ TreeTime/AVLTreeAPI.java
@@ -1,19 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Describes operations on an AVLTree
+ * @param <E> the data type
+ * @see AVLTreeException
  */
 package TreeTime;
 
 import java.util.function.Function;
 
-/**
- * Describes operations on an AVLTree
- * @param <E> the data type
- * @author William Duncan
- * @sinc 99-99-9999
- * @see AVLTreeException
- */
+
 public interface AVLTreeAPI<E extends Comparable<E>>
 {
    /**
